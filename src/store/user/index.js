7d7ab6192beb737a1091c7502f27e17c5d08d241
Camden Ko <camdenko@gmail.com>
2017-09-22 15:43:43 -0500

    remove User test

--- src/store/user/index.js
+++ src/store/user/index.js
@@ -1,5 +1,5 @@
-const GET_USER = 'GET_USER'
-const REMOVE_USER = 'REMOVE_USER'
+export const GET_USER = 'GET_USER'
+export const REMOVE_USER = 'REMOVE_USER'
 
 export const getUser = user => ({ type: GET_USER, user })
 export const removeUser = () => ({ type: REMOVE_USER })
--- src/store/user/user.test.js
+++ src/store/user/user.test.js
@@ -1,11 +1,15 @@
 import * as userRedux from './index'
+
 describe('User Action Creators', () => {
   test('getUser', () => {
     const user = {
       id: '1234',
       name: 'name',
     }
-    expect(userRedux.getUser(user)).toEqual({ type: 'GET_USER', user })
+    expect(userRedux.getUser(user)).toEqual({ type: userRedux.GET_USER, user })
   })
 
+  test('removeUser', () => {
+    expect(userRedux.removeUser()).toEqual({ type: userRedux.REMOVE_USER })
+  })
 })
