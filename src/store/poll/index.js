601f689b93c63d003ea63e4dfc4603c451d70d87
Camden Ko <camdenko@gmail.com>
2017-09-26 16:36:04 -0500

    getPoll now sends out pollRequested()

--- src/store/poll/index.js
+++ src/store/poll/index.js
@@ -15,11 +15,13 @@ export const updatePoll = poll => ({ type: UPDATE_POLL, poll })
 export const deletePoll = () => ({ type: DELETE_POLL })
 export const pollRequested = () => ({ type: POLL_REQUESTED })
 
-export const getPoll = dispatch =>
+export const getPoll = (dispatch) => {
+  dispatch(pollRequested())
   database.ref('/polls').once(
     'value',
     snap => dispatch(readPoll(snap.val())),
   )
+}
 
 export default (state = defaultState, action) => {
   switch (action.type) {
