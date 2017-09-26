c8c97d8dd4559d9a98e1d20477cc16a05eb90653
Camden Ko <camdenko@gmail.com>
2017-09-26 09:56:58 -0500

    loginform organism and scaffolding

---
+++ src/components/Organisms/LoginForm.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {
+
+} from 'react-native'
+
+import {
+  LabelledInput,
+} from '../../components'
+
+export default () => (
+  <LabelledInput label="Email" />
+)
