6e947fe51550ec32da38701836ea724e20648e5d
Camden Ko <camdenko@gmail.com>
2017-09-26 11:53:39 -0500

    sign up inputs

--- src/components/Organisms/LoginSignUp/index.js
+++ src/components/Organisms/LoginSignUp/index.js
@@ -1,1 +1,4 @@
 export { default as LoginInputs } from './LoginInputs/LoginInputs'
+export { default as LoginButtons } from './LoginButtons/LoginButtons'
+export { default as SignupInputs } from './SignupInputs/SignupInputs'
+export { default as SignupButtons } from './SignupButtons/SignupButtons'
