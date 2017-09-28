599915b714e86aacac66bea39fa3037fb9f8e97d
Camden Ko <camdenko@gmail.com>
2017-09-28 17:05:47 -0500

    extended defaultoptions for creating poll

--- src/store/poll/utilities.js
+++ src/store/poll/utilities.js
@@ -21,17 +21,35 @@ const recursiveGenerateHash = (usedNums) => {
 export const getHash = async () => {
   const pollSnap = await database.ref('/polls').once('value')
   const pollVal = pollSnap.val()
-  const usedNums = new Set(Object.keys(pollVal).map(str => parseInt(str, 10)))
+  const usedNums = new Set(
+    Object.keys(pollVal)
+      .map(str => parseInt(str, 10)),
+  )
   return recursiveGenerateHash(usedNums)
+}
+
+const convertMinutesToMS = minutes =>
+  minutes * 60 * 1000
+
+const generateDefaultOptions = () => {
+  const defaultDelay = 15
+  return {
+    endTime: Date.now() + convertMinutesToMS(defaultDelay),
+    open: false,
+  }
 }
 
 export default {
   createPoll: async () => {
     try {
       const hash = await getHash()
-      const toPush = {
-        [hash]: 'sad',
-      }
+      const defaultOptions = generateDefaultOptions()
+      const toPush = Object.assign(
+        defaultOptions,
+        {
+          [hash]: 'sad',
+        },
+      )
       return await database.ref('/polls').update(toPush)
     } catch (error) {
       return error
