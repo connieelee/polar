d8c802621b125ceb0e1d309098e164dfc6687b2e
Camden Ko <camdenko@gmail.com>
2017-09-28 16:07:21 -0500

    generate a polls hash and push to firebase

--- src/store/poll/utilities.js
+++ src/store/poll/utilities.js
@@ -1,5 +1,40 @@
-export default {
-  generatePoll: (lastPollId) => {
+import { database } from '../../../firebase'
 
+const generateRandomNum = () =>
+  Math.floor(Math.random() * 4) + 1
+
+export const generateHash = () =>
+  parseInt([1, 1, 1, 1]
+    .map(() => generateRandomNum().toString())
+    .join(''),
+  10,
+  )
+
+export const objectNotEmpty = obj =>
+  !!Object.keys(obj).length
+
+const recursiveGenerateHash = (usedNums) => {
+  const hash = generateHash()
+  return usedNums.has(hash) ? recursiveGenerateHash(usedNums) : hash
+}
+
+export const getHash = async () => {
+  const pollSnap = await database.ref('/polls').once('value')
+  const pollVal = pollSnap.val()
+  const usedNums = new Set(Object.keys(pollVal).map(str => parseInt(str, 10)))
+  return recursiveGenerateHash(usedNums)
+}
+
+export default {
+  createPoll: async () => {
+    const hash = await getHash()
+    const toPush = {
+      [hash]: 'sad',
+    }
+    try {
+      return await database.ref('/polls').update(toPush)
+    } catch (error) {
+      return error
+    }
   },
 }
---
+++ src/store/poll/utilities.test.js
@@ -0,0 +1,44 @@
+import defaultUtilities, * as utilities from './utilities'
+import { database } from '../../../firebase'
+
+afterAll(() => database.goOffline())
+
+describe('Utilities', () => {
+  describe('Generate hash', () => {
+    test('it only creates integers 1-4', () => {
+      const hashed = utilities.generateHash()
+      hashed
+        .toString()
+        .split('')
+        .map(str => parseInt(str, 10))
+        .forEach((num) => {
+          expect(num).toBeGreaterThanOrEqual(1)
+          expect(num).toBeLessThanOrEqual(4)
+        })
+    })
+    test('it generates a number', () =>
+      expect(typeof utilities.generateHash()).toBe('number'),
+    )
+    test('it generates a number of length four', () =>
+      expect(utilities.generateHash().toString().length).toBe(4),
+    )
+  })
+
+  describe('objectNotEmpty', () => {
+    test('it returns false if an object isn\'t empty', () => {
+      expect(utilities.objectNotEmpty({ a: true })).toBe(true)
+    })
+    test('it returns false if object is empty', () => {
+      expect(utilities.objectNotEmpty({})).toBe(false)
+    })
+  })
+  describe('getHash', () => {
+    test('generates a valid hash', async () => {
+      const hash = await utilities.getHash()
+      await defaultUtilities.createPoll()
+      expect(hash.toString().length).toBe(4)
+      expect(typeof hash).toBe('number')
+    })
+  })
+})
+
