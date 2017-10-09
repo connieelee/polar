6d87d04171b565db2aa6f01391c2402e9792a941
Camden Ko <camdenko@gmail.com>
2017-10-08 19:14:21 -0500

    changed hash to keys

--- src/store/poll/utilities.js
+++ src/store/poll/utilities.js
@@ -3,29 +3,24 @@ import { database } from '../../../firebase'
 const generateRandomNum = () =>
   Math.floor(Math.random() * 4) + 1
 
-export const generateHash = () =>
-  parseInt([1, 1, 1, 1]
+export const generateKey = () =>
+  [1, 1, 1, 1]
     .map(() => generateRandomNum().toString())
-    .join(''),
-  10,
-  )
+    .join('')
 
 export const objectNotEmpty = obj =>
   !!Object.keys(obj).length
 
-const recursiveGenerateHash = (usedNums) => {
-  const hash = generateHash()
-  return usedNums.has(hash) ? recursiveGenerateHash(usedNums) : hash
+const recursiveGenerateKey = (usedNums) => {
+  const key = generateKey()
+  return usedNums.has(key) ? recursiveGenerateKey(usedNums) : key
 }
 
-export const getHash = async () => {
+export const getKey = async () => {
   const pollSnap = await database.ref('/polls').once('value')
   const pollVal = pollSnap.val()
-  const usedNums = new Set(
-    Object.keys(pollVal)
-      .map(str => parseInt(str, 10)),
-  )
-  return recursiveGenerateHash(usedNums)
+  const usedNums = new Set(Object.keys(pollVal))
+  return recursiveGenerateKey(usedNums)
 }
 
 const convertMinutesToMS = minutes =>
@@ -36,13 +31,12 @@ const generateDefaultOptions = (delayMin = 15, open = false) => ({
   open,
 })
 
-
 export default {
   createPoll: async () => {
     try {
-      const hash = await getHash()
+      const key = await getKey()
       const toPush = {
-        [hash]: generateDefaultOptions(),
+        [key]: generateDefaultOptions(),
       }
       return await database.ref('/polls').update(toPush)
     } catch (error) {
