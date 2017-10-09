ba86fd66d87c2b5b78e4a1c89a300774ae0120ee
Camden Ko <camdenko@gmail.com>
2017-10-09 15:00:32 -0500

    tinybutton files

--- storybook/storyLoader.js
+++ storybook/storyLoader.js
@@ -10,7 +10,7 @@ function loadStories() {
   require('../src/components/Atoms/Form/TextInput/TextInput.stories.js');
   require('../src/components/Atoms/Typography/BodyText/BodyText.stories.js');
   require('../src/components/Atoms/Typography/FormLabel/FormLabel.stories.js');
-  require('../src/components/Molecules/Form/AddQuestions/AddQuestions.stories.js');
+  require('../src/components/Molecules/Form/AddAnswers/AddAnswers.stories.js');
   require('../src/components/Molecules/Form/LabeledInput/LabeledInput.stories.js');
   require('../src/components/Organisms/CreatePoll/CreatePollForm/CreatePollForm.stories.js');
   require('../src/components/Organisms/LoginSignUp/LoginButtons/LoginButtons.stories.js');
