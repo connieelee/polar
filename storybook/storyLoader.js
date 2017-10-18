2cd57752b2c8cea76a3ee66adf12c1d251d6a59b
Connie Lee <connie.lee@fullstackacademy.com>
2017-10-18 10:01:46 -0500

    Fixed unrecognized font issue

--- storybook/storyLoader.js
+++ storybook/storyLoader.js
@@ -5,7 +5,6 @@
 // https://github.com/elderfo/react-native-storybook-loader.git
 
 function loadStories() {
-  
   require('../src/components/Atoms/Form/FormButton/FormButton.stories.js');
   require('../src/components/Atoms/Form/TextInput/TextInput.stories.js');
   require('../src/components/Atoms/Typography/BodyText/BodyText.stories.js');
@@ -18,6 +17,20 @@ function loadStories() {
   
 }
 
+const stories = [
+  '../src/components/Atoms/Form/FormButton/FormButton.stories.js',
+  '../src/components/Atoms/Form/TextInput/TextInput.stories.js',
+  '../src/components/Atoms/Typography/BodyText/BodyText.stories.js',
+  '../src/components/Atoms/Typography/FormLabel/FormLabel.stories.js',
+  '../src/components/Molecules/Form/LabelledInput/LabelledInput.stories.js',
+  '../src/components/Organisms/LoginSignUp/LoginButtons/LoginButtons.stories.js',
+  '../src/components/Organisms/LoginSignUp/LoginInputs/LoginInputs.stories.js',
+  '../src/components/Organisms/LoginSignUp/SignupButtons/SignupButtons.stories.js',
+  '../src/components/Organisms/LoginSignUp/SignupInputs/SignupInputs.stories.js',
+  
+];
+
 module.exports = {
   loadStories,
+  stories,
 };
